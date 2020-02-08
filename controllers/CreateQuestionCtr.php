3ff75c20c1fb9e61b17faacf2fae200edb71a1e9
gabo <gabo.garces96@hotmail.com>
2020-02-08 19:54:26 +0100

    fixed position of $dbs and $themes

--- controllers/CreateQuestionCtr.php
+++ controllers/CreateQuestionCtr.php
@@ -3,25 +3,26 @@
 session_start();
 $_SESSION["page"] = $_SERVER["REQUEST_URI"];
 require_once("../model/SQLQuiz.php");
+require_once("../model/Quiz_DB.php");
+require_once("../model/Theme.php");
+$dbs = Quiz_DB::searchAll();
+$themes = Theme::getAll();
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
         $bs = array();
-        require_once("../model/Quiz_DB.php");
-        require_once("../model/Theme.php");
-        $dbs = Quiz_DB::searchAll();
-        $themes = Theme::getAll();
         require_once("../views/createQuestionView.php");
         break;
     case "POST":
-        $data = array(
-            $db_name = filter_input(INPUT_POST, "db_name", FILTER_SANITIZE_STRING),
-            $question_text = filter_input(INPUT_POST, "question_text", FILTER_SANITIZE_STRING),
-            $correct_answer = filter_input(INPUT_POST, "correct_answer", FILTER_SANITIZE_STRING),
-            //$correct_result = filter_input(INPUT_POST, "correct_result", FILTER_SANITIZE_STRING),
-            $is_public = filter_input(INPUT_POST, "is_public", FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE),
-            $theme_id = filter_input(INPUT_POST, "theme_id", FILTER_VALIDATE_INT)
+        $question = array(
+            "db_name" => filter_input(INPUT_POST, "db_name", FILTER_SANITIZE_STRING),
+            "question_text" => filter_input(INPUT_POST, "question_text", FILTER_SANITIZE_STRING),
+            "correct_answer" => filter_input(INPUT_POST, "correct_answer", FILTER_SANITIZE_STRING),
+            //"correct_result" => filter_input(INPUT_POST, "correct_result", FILTER_SANITIZE_STRING),
+            "is_public" => filter_input(INPUT_POST, "is_public", FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE),
+            "theme_id" => filter_input(INPUT_POST, "theme_id", FILTER_VALIDATE_INT)
         );
-        $test = testSQL($correct_answer, $db_name);
+        $test = testSQL($question["correct_answer"], $question["db_name"]);
+        require_once("../views/createQuestionView.php");
         if($test["PASS"]){
             //implode results
             //insert to database
