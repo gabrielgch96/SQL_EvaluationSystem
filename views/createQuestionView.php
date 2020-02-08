b3b68d9df2195dc4035bde71d8817a6943f02bce
gabo <gabo.garces96@hotmail.com>
2020-02-08 19:54:48 +0100

    returns values  user input if errors

--- views/createQuestionView.php
+++ views/createQuestionView.php
@@ -1,5 +1,14 @@
 <?php
 //data: $dbs
+if(!isset($question)){
+    $question = array(
+        "db_name" => $dbs[0]["db_name"],
+        "question_text" => "",
+        "correct_answer" => "",
+        "is_public" => false,
+        "theme_id" => $themes[0]["theme_id"]
+    );
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,23 +22,25 @@
         <select form="questionForm" name="db_name">
         <?php
             foreach($dbs as $row){
-                echo '<option value="'.$row["db_name"].'">'.$row["db_name"].'</option>';
+                $v = strcmp($question["db_name"],$row["db_name"])==0? "selected":"";
+                echo '<option value="'.$row["db_name"].'"'.$v.'>'.$row["db_name"].'</option>';
            }
         ?>
         </select>
         <label>Question text:</label>
-        <input type="text" name="question_text">
+        <input type="text" name="question_text" value="<?= $question["question_text"]?>">
         <label>Correct Answer:</label>
-        <input type="text" name="correct_answer">
+        <input type="text" name="correct_answer" value="<?= $question["correct_answer"]?>">
         <!--<label>Correct Result: </label>
         <input type=text name="correct_result">-->
         <label>Public?</label>
-        <input type="checkbox" name="is_public">
+        <input type="checkbox" name="is_public" value="<?= $question["is_public"]?>">
         <label>Theme</label>
         <select form="questionForm" name="theme_id">
         <?php
             foreach($themes as $row){
-                echo '<option value="'.$row["theme_id"].'">'.$row["label"].'</option>';
+                $v = $question["theme_id"]==$row["theme_id"]? "selected":"";
+                echo '<option value="'.$row["theme_id"].'" '.$v.'>'.$row["label"].'</option>';
            }
         ?>
         </select>
