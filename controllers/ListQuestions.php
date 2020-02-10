3d0aa50f1a1055c9d85e516573b57b0b7cb8469f
gabo <gabo.garces96@hotmail.com>
2020-02-10 16:47:04 +0100

    added all filter option

--- controllers/ListQuestions.php
+++ controllers/ListQuestions.php
@@ -9,10 +9,13 @@ $dbs = Quiz_DB::searchAll();
 switch($_SERVER["REQUEST_METHOD"]){
     case "GET":
         $questions = SQLQuestion::getWithDisplayFormat();
+        $db_name = "all";
         require_once("../views/questions_view.php");
         break;
     case "POST":
         $db_name = filter_input(INPUT_POST, "db_name", FILTER_SANITIZE_STRING);
+        if(strcmp($db_name, "all")==0)
+            $db_name = null;
         $questions = SQLQuestion::getWithDisplayFormat($db_name);
         require_once("../views/questions_view.php");
         break;
