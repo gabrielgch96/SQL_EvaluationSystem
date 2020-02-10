c89c953fdb4ff47904153251c30351091a097a03
gabo <gabo.garces96@hotmail.com>
2020-02-10 15:42:29 +0100

    added filter func and request method difference

--- controllers/ListQuestions.php
+++ controllers/ListQuestions.php
@@ -1,7 +1,22 @@
 <?php
 session_start();
 require_once("../model/SQLQuestion.php");
+require_once("../model/Quiz_DB.php");
 $questions = array();
-$questions = SQLQuestion::search();
-require_once("../views/questions_view.php");
+$dbs = array();
+$dbs = Quiz_DB::searchAll();
+
+switch($_SERVER["REQUEST_METHOD"]){
+    case "GET":
+        $questions = SQLQuestion::getWithDisplayFormat();
+        require_once("../views/questions_view.php");
+        break;
+    case "POST":
+        $db_name = filter_input(INPUT_POST, "db_name", FILTER_SANITIZE_STRING);
+        $questions = SQLQuestion::getWithDisplayFormat($db_name);
+        require_once("../views/questions_view.php");
+        break;
+    default:
+        die("Method Not Implemented");
+}
 ?>
